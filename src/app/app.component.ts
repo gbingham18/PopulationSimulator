8e6a9c915c3ac13d550868ed7462b3b882192f95
Jake Geldbach <jgeldbach18@my.whitworth.edu>
2018-04-21 10:31:52 -0700

    Insert graph
    Found graphing js library that we are going to use, and put in a template graph for first sprint demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Img}  from 'react-image'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +8,7 @@ import {Img}  from 'react-image'
 export class AppComponent {
   title = 'Population Simulator';
   projDesc = 'Here is an description about how the population simulations on this website will work.'
-}
 
+
+}
 
